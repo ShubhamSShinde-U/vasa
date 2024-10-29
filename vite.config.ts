7f8e728c7f2219e38bf751e5dd8ab31eeebc29a1
Shubham Shinde <shubhamshindec1@gmail.com>
2024-10-29 11:53:57 +0530

    asda

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // This will allow the server to be publicly accessible
+    port: 3000,       // Set a port (Render usually uses port 3000 by default)
+  },
 })
