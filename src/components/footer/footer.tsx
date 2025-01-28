56ae24b74025c814db888657ffd4aeed935b0381
Shubham Shinde <shubhamshindec1@gmail.com>
2025-01-29 00:46:18 +0530

    changes

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,11 +1,35 @@
+import { useNavigate } from "react-router-dom";
 import "./footer.scss";
 function Footer() {
+  const navigate = useNavigate();
+  const handleClick=()=>{
+      window.open(
+        "https://www.linkedin.com/company/vasa-extreme-engineering",
+        "_blank",
+        "noopener,noreferrer"
+      );
+
+  }
   return (
     <div className="footer-wrapper">
       <div className="footer">
         <div className="footer-above">
-          Services |
-          <img src="assets/linkedin.svg" alt="" width={25} />
+          <div
+            onClick={() => {
+              navigate("/services");
+            }}
+          >
+            Services
+          </div>{" "}
+          |
+          <img
+            src="assets/linkedin.svg"
+            alt=""
+            width={25}
+            onClick={() => {
+              handleClick();
+            }}
+          />
         </div>
         <div className="footer-below">
           Copyright © 2024 VASA Extreme Engineering Ltd. All rights reserved.
